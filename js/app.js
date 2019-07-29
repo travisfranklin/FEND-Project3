dbf42666f51e92cf5f4e98ba0872ffb7497783ce
travisfranklin <travislf88@gmail.com>
2019-07-29 14:02:11 -0500

    Player and Enemies completed, add input handler

--- js/app.js
+++ js/app.js
@@ -1,50 +1,100 @@
-// Enemies our player must avoid
-var Enemy = function(x, y, s) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+// Create the Enemy class
+const Enemy = function(x, y, s) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = s;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// update() method handles Enemy screen movement and location.
+// Input: dt, a time delta between ticks
+// Output: randomized enemy speed amount 
+// Caveat: when an enemy reaches the right side of the canvas, or touches the player, they must reset with a randomized speed
+const randomSpeed = Math.floor(Math.random() * 4 + 1);
+
 Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
 
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if (this.x > 707) {
+        this.x = -100;
+        this.speed = 10 * randomSpeed;
+    }
+
+    // Create Enemy hitboxes
+    // Input: player position, Enemy position, Enemy size 
+
+
 };
 
-// Draw the enemy on the screen, required method for game
+// render() method shows enemy sprite
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-let Player = function(x, y) {
+// Create the player class
+const Player = function(x, y) {
     this.sprite = 'images/char-cat-girl.png';
     this.x = 303;
     this.y = 404;
-    this.x_movement = 101;
-    this.y_movement = 83;
+    this.x_mov = 101;
+    this.y_mov = 83;
 }
 
+// render() method shows player sprite
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// update() method
+Player.prototype.update = function(dt) {
+
+
+}
+
+// reset() method
+// Input: Goal reached, or enemy touched.
+// Output: Player sprite moves back to the start.
+Player.prototype.reset = function() {
+    this.x = 303;
+    this.y = 404;
+}
+
+
+// handleInput() method 
+// Input: key presses from the EventListener.
+// Output: Player sprite moves in each key's cardinal direction. 
+// Caveat: Player sprite can't move out of bounds.
+Player.prototype.handleInput = function(direction) {
+    switch (direction) {
+        case 'left':
+            this.x >= this.x_mov ? this.x -= this.x_mov : this.x -= 0;
+            break;
+        case 'right':
+            this.x <= (this.x_mov * 5) ? this.x += this.x_mov : this.x += 0;
+            break;
+        case 'up':
+            if (this.y <= 50) {
+                crossed++;
+                this.reset();
+            }
+            break;
+        case 'down':
+            this.y <= (this.y_mov * 4) ? this.y += this.y_mov : this.y += 0;
+            break;
+    }
+}
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+let enemy0 = new Enemy(-80, 60 + 80 * 0, randomSpeed);
+let enemy1 = new Enemy(-80, 60 + 80 * 1, randomSpeed);
+let enemy2 = new Enemy(-80, 60 + 80 * 2, randomSpeed);
+window.allEnemies = [enemy1, enemy2, enemy3];
 
+// Place the player object in a variable called player
+window.player = new Player();
 
 
 // This listens for key presses and sends the keys to your
